e54b6576612a4d82d017ef31fea66c7d99fa2b6f
Alan Hortz <alan.hortz@gmail.com>
2013-04-01 10:12:13 +0200

    Add spec for big discount of 25% if 5 books have been ordered

--- spec/BasketSpec.js
+++ spec/BasketSpec.js
@@ -2,6 +2,9 @@ describe("Basket", function() {
 	var basket;
 	var book1 = new Book(1,'Harry Potter tome 1',8);
 	var book2 = new Book(2,'Harry Potter tome 2',8);
+	var book3 = new Book(1,'Harry Potter tome 3',8);
+	var book4 = new Book(2,'Harry Potter tome 4',8);
+	var book5 = new Book(2,'Harry Potter tome 5',8);
 
  	beforeEach(function() {
   		basket = new Basket();
@@ -60,6 +63,22 @@ describe("Basket", function() {
 
 		it("should indicates that a discount of 5% has been applied on the amount of the checkout", function() {
 			expect(basket.checkout()).toBe(8 * 2 * 0.95);
+		});
+	});
+
+	describe("when the five tome have been added",function() {
+
+		beforeEach(function() {
+			basket.add(book1);
+			basket.add(book2);
+			basket.add(book3);
+			basket.add(book4);
+			basket.add(book5);
+
+		});
+
+		it("should indicates that a huge discount of 25% has been applied on the amount of the checkout", function() {
+			expect(basket.checkout()).toBe(8 * 5 * 0.75);
 		});
 	});
 
